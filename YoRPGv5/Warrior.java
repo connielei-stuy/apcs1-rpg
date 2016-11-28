dd851f7a6652cb0c81b74c372a4e172fab8b0f9c
clei1 <clei1@stuy.edu>
2016-11-27 19:29:15 -0500

    Update Warrior.java

--- YoRPGv5/Warrior.java
+++ YoRPGv5/Warrior.java
@@ -12,6 +12,11 @@ public class Warrior extends Character{
 	defense = 90;
     }
     
+    public void specialize(){
+	strength = 85;
+	defense = 85;
+    }
+		
     public String about(){
 	return	"Warrior\nHealth: " + health + "\nStrength: " + strength + "\nDefense: " + defense + "\nAttack Rating: " + attackR;
     }
