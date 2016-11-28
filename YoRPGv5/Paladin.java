8b0a6fce3e54652cc1e070473a35d425216d3395
clei1 <clei1@stuy.edu>
2016-11-27 19:25:32 -0500

    Update Paladin.java

--- YoRPGv5/Paladin.java
+++ YoRPGv5/Paladin.java
@@ -12,6 +12,11 @@ public class Paladin extends Character{
 	defense = 90;
     }
 	
+    public void specialize(){
+	strength = 85;
+	defense = 85;
+    }	    
+	    
     public String about(){
 	return "Paladin\nHealth: " + health + "\nStrength: " + strength + "\nDefense: " + defense + "\nAttack Rating: " + attackR;
     }
